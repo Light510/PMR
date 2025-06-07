0e8ad32b37152a24e38fa8e732848e6c2df2f996
Atta <attaclp@gmail.com>
2025-06-07 08:53:35 +0700

    updated database

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 // db.js
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("students.db");
+const path = require("path");
+const db = new sqlite3.Database(path.join(__dirname, "students.db"));
 
 // Buat tabel jika belum ada
 db.serialize(() => {
